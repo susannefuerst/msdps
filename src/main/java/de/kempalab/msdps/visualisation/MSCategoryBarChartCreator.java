4ac2ae62dcf1a8fd9902553bc43dd297ff6502ca
susannefuerst <susannefuerst@freenet.de>
2018-11-21 17:23:14 +0100

    changed chart style

--- src/main/java/de/kempalab/msdps/visualisation/MSCategoryBarChartCreator.java
+++ src/main/java/de/kempalab/msdps/visualisation/MSCategoryBarChartCreator.java
@@ -1,14 +1,24 @@
 package de.kempalab.msdps.visualisation;
 
+import java.awt.Color;
 import java.awt.Font;
+import java.awt.GradientPaint;
+import java.text.NumberFormat;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.ExtendedCategoryAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.ItemLabelAnchor;
+import org.jfree.chart.labels.ItemLabelPosition;
+import org.jfree.chart.labels.StandardCategoryItemLabelGenerator;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.ui.TextAnchor;
 
 import de.kempalab.msdps.MSDatabase;
 import de.kempalab.msdps.MSShiftDatabase;
@@ -31,37 +41,6 @@ public class MSCategoryBarChartCreator {
 	}
 	
 	/**
-	 * Creates a category bar chart where the natural, marked and mixed spectrum from the {@link MSDatabase} is included.
-	 * Masses are realized as categories. If the {@link MSDatabase} is an instance of a {@link MSShiftDatabase} there will
-	 * be a tool tip for each bar that represents the incorporated isotopes at this point.
-	 * @param msDatabase
-	 * @return a {@link JFreeChart} that may be used for visualization of the spectra in the {@link MSDatabase}.
-	 */
-	public static JFreeChart createCombinedMSBarChart(MSDatabase msDatabase) {
-		MSCategoryDataset spectraDataset = new MSCategoryDataset(msDatabase, MSBarChartType.ALL_SPECTRA);
-		String title = "Fragment: " + msDatabase.getFragmentKey() + "_" + msDatabase.getFragmentFormula() + ", incorporated tracer: " + msDatabase.getIncorporatedTracers();
-		JFreeChart chart = ChartFactory.createBarChart(title, CHART_X_LABEL , CHART_Y_LABEL, spectraDataset,
-				PlotOrientation.VERTICAL, true, true, false);
-		CategoryPlot categoryPlot = chart.getCategoryPlot();
-		BarRenderer renderer = (BarRenderer) categoryPlot.getRenderer();
-		renderer.setMaximumBarWidth(MAX_BAR_WIDTH); // set maximum width relative to chart;
-		renderer.setBaseToolTipGenerator(new MSCategoryToolTipGenerator());
-		ExtendedCategoryAxis domainAxis = new ExtendedCategoryAxis(null);
-		categoryPlot.setDomainAxis(domainAxis);
-		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_45);
-		MSShiftDatabase msshiftDatabase = (MSShiftDatabase) spectraDataset.getMsDatabase();
-		if (!(msshiftDatabase.includesMarkedSpectrum())/*the chart only contains a natural spectrum */) {
-			for (Object massCategoryObject : spectraDataset.getColumnKeys()) {
-				Double mass = (Double) massCategoryObject;
-				String isotopesString = msshiftDatabase.shiftInducingIsotopes(IncorporationType.NATURAL, mass);
-				domainAxis.addSubLabel(mass, isotopesString);
-			}
-			
-		}
-		return chart;
-	}
-	
-	/**
 	 * Creates a category bar chart where the spectra (all or single ones, depending on msBarChartType) from the {@link MSDatabase} are included.
 	 * Masses are realized as categories. If the {@link MSDatabase} is an instance of a {@link MSShiftDatabase} there will
 	 * be a tool tip for each bar that represents the incorporated isotopes at this point.
@@ -71,16 +50,29 @@ public class MSCategoryBarChartCreator {
 	 */
 	public static JFreeChart createMSBarChart(MSDatabase msDatabase, MSBarChartType msBarChartType) {
 		MSCategoryDataset spectraDataset = new MSCategoryDataset(msDatabase, msBarChartType);
-		String title = "Fragment: " + msDatabase.getFragmentKey() + "_" + msDatabase.getFragmentFormula() + ", incorporated tracer: " + msDatabase.getIncorporatedTracers();
+		String title = "Fragment: " + msDatabase.getFragmentKey() + "_" + msDatabase.getFragmentFormula();
+		String subtitle = "incorporated tracer: " + msDatabase.getIncorporatedTracers();
 		JFreeChart chart = ChartFactory.createBarChart(title, CHART_X_LABEL , CHART_Y_LABEL, spectraDataset,
 				PlotOrientation.VERTICAL, true, true, false);
+		chart.addSubtitle(new TextTitle(subtitle));
+		ExtendedCategoryAxis domainAxis = new ExtendedCategoryAxis(null);
+		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_45);
 		CategoryPlot categoryPlot = chart.getCategoryPlot();
+		categoryPlot.setDomainAxis(domainAxis);
+		categoryPlot.setBackgroundPaint(Color.white);
 		BarRenderer renderer = (BarRenderer) categoryPlot.getRenderer();
 		renderer.setMaximumBarWidth(MAX_BAR_WIDTH); // set maximum width relative to chart;
 		renderer.setBaseToolTipGenerator(new MSCategoryToolTipGenerator());
-		ExtendedCategoryAxis domainAxis = new ExtendedCategoryAxis(null);
-		categoryPlot.setDomainAxis(domainAxis);
-		domainAxis.setCategoryLabelPositions(CategoryLabelPositions.DOWN_45);
+		renderer.setBaseItemLabelGenerator(new StandardCategoryItemLabelGenerator());
+        renderer.setBaseItemLabelsVisible(true);
+        renderer.setBaseItemLabelPaint(Color.black);
+        renderer.setBasePositiveItemLabelPosition(new ItemLabelPosition(ItemLabelAnchor.OUTSIDE12, TextAnchor.BOTTOM_CENTER, TextAnchor.CENTER, Math.PI / -2.0));
+        renderer.setItemLabelAnchorOffset(20);
+        renderer.setSeriesPaint(0, new Color(0, 0, 0)/*black*/);
+        renderer.setSeriesPaint(1, new Color(51, 102, 153)/*blue*/);
+        renderer.setSeriesPaint(2, new Color(102, 102, 153)/*purple*/);
+        NumberAxis rangeAxis = (NumberAxis) categoryPlot.getRangeAxis();
+        rangeAxis.setUpperMargin(0.10);
 		MSShiftDatabase msshiftDatabase = (MSShiftDatabase) spectraDataset.getMsDatabase();
 		if (!msBarChartType.equals(MSBarChartType.ALL_SPECTRA)) {
 			for (Object massCategoryObject : spectraDataset.getColumnKeys()) {
@@ -100,5 +92,4 @@ public class MSCategoryBarChartCreator {
 		}
 		return chart;
 	}
-
 }
