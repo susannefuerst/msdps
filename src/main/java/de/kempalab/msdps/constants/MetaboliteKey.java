e65716b99ccf830db5ba92583e25a60bd513d35c
susannefuerst <susannefuerst@freenet.de>
2019-01-11 13:14:22 +0100

    updated fragment and metabolite keys

--- src/main/java/de/kempalab/msdps/constants/MetaboliteKey.java
+++ src/main/java/de/kempalab/msdps/constants/MetaboliteKey.java
@@ -2,16 +2,36 @@ package de.kempalab.msdps.constants;
 
 public enum MetaboliteKey {
 	
-	LAC("Lactic acid", "Lac"),
-	ASP("Aspartic acid", "Asp"),
-	MAL("Malic acid", "Mal"),
-	GLC("Glucose_MP / BP", "Glc"),
-	ALA("Alanine", "Ala"),
-	GLY("Glycine", "Gly"),
-	SER("Serine", "Ser"),
-	GLN("Glutamine", "Gln"),
-	UNKNOWN("Unknown", "UNKNOWN"),
-	;
+	ALA("Alanine", "Ala"), //
+	ASP("Aspartic acid", "Asp"), //
+	CIT("Citric acid", "Cit"), //
+	DHAP("Dihydroxyacetone phosphate", "DHAP"), //
+	FRC("Fructose_MP", "Frc"), //
+	FBP("Fructose-1_6-diphosphate", "FBP"), //
+	F6P("Fructose-6-phosphate", "F6P"), //
+	FUM("Fumaric acid", "Fum"), //
+	PGA6("Gluconic acid-6-phosphate", "6PGA"), //
+	GLC("Glucose_MP / BP", "Glc"), //
+	G16P("Glucose-1/6-phosphate", "G1/6P"), //
+	G6P("Glucose-6-phosphate", "G6P"), //
+	GLU("Glutamic acid", "Glu"), //
+	GLN("Glutamine", "Gln"), //
+	HG2("Glutaric acid_ 2-hydroxy-", "2HG"), //
+	AKG("Glutaric acid_ 2-oxo-", "aKG"), //
+	GLT("Glutaric acid", "Glt"), //
+	PGA3("Glyceric acid-3-phosphate", "3PGA"), //
+	GLYC("Glycerol", "Glyc"), //
+	GLYC3P("Glycerol-3-phosphate", "Glyc3P"), //
+	GLY("Glycine", "Gly"), //
+	LAC("Lactic acid", "Lac"), //
+	MAL("Malic acid", "Mal"), //
+	PEP("Phosphoenolpyruvic acid", "PEP"), //
+	PYR("Pyruvic acid", "Pyr"), //
+	R5P("Ribose-5-phosphate", "R5P"), //
+	SER("Serine", "Ser"), //
+	SUC("Succinic acid", "Suc"), //
+	UNKNOWN("Unknown", "UNKNOWN"),//
+	    ;
 	private String moleculeName;
 	private String abbreviation;
 	
