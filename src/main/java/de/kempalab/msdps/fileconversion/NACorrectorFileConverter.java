124f48c1b9a8b58dc1d4de83516c4b11907fde70
Fürst <sufuers@mdc-berlin.net>
2019-01-24 16:52:14 +0100

    first adaptions

--- src/main/java/de/kempalab/msdps/fileconversion/NACorrectorFileConverter.java
+++ src/main/java/de/kempalab/msdps/fileconversion/NACorrectorFileConverter.java
@@ -2,7 +2,10 @@ package de.kempalab.msdps.fileconversion;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -14,7 +17,7 @@ import de.kempalab.msdps.data.DataTable;
 import de.kempalab.msdps.util.FileWriterUtils;
 
 public class NACorrectorFileConverter {
-	
+
 	/**
 	 * Converts a csv file with entries according to {@link MZminePeakOutputHeader} to a csv file with entries according to {@link NACorrectionInputHeader}.
 	 * This file can subsequently be used for natural abundance correction using {@link NACorrector}.
@@ -74,7 +77,7 @@ public class NACorrectorFileConverter {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Converts an array of csv files with entries according to {@link MZminePeakOutputHeader} to a csv file with entries according to {@link NACorrectionInputHeader}.
 	 * This file can subsequently be used for natural abundance correction using {@link NACorrector}.
@@ -88,56 +91,74 @@ public class NACorrectorFileConverter {
 				NACorrectionInputHeader.C_13_COUNT.getHeaderValue(),
 				NACorrectionInputHeader.N_15_COUNT.getHeaderValue(),
 				NACorrectionInputHeader.FORMULA.getHeaderValue());
-			try {
-				for (String mzMineOutpuFilePath : mzMineOutpuFilePaths) {
-					File mzMinePeakData = new File(mzMineOutpuFilePath);
-					CSVParser parser;
-					parser = CSVParser.parse(mzMinePeakData, Charset.defaultCharset(), CSVFormat.RFC4180);
-					List<CSVRecord> records = parser.getRecords();
-					String filename = "";
-					String metaboliteFragment = "";
-					String formula = "";
-					int c13Count;
-					int n15Count;
-					String groupKey = "";
-					double[][] singleIntensities = new double[10][10];
-					for (CSVRecord csvRecord : records) {
-						if (csvRecord.getRecordNumber() == 1) {
-							String peakHeightHeader = csvRecord.get(MZminePeakOutputHeader.PEAK_HEIGHT.getColumnNumber());
-							filename = StringUtils.remove(peakHeightHeader, MZminePeakOutputHeader.PEAK_HEIGHT.getHeaderValue());
-							continue;
-						}
-						String detailStr = csvRecord.get(MZminePeakOutputHeader.SEPARATED_DETAILS.getColumnNumber());
-						String[] details = StringUtils.split(detailStr, ";");
-						formula = StringUtils.remove(details[MZmineDetailsIdentfier.FORMULA.getIndex()], MZmineDetailsIdentfier.FORMULA.getIdentifier());
-						metaboliteFragment = StringUtils.remove(details[MZmineDetailsIdentfier.ID.getIndex()], MZmineDetailsIdentfier.ID.getIdentifier());
-						metaboliteFragment = StringUtils.substringBefore(metaboliteFragment, "_");
-						groupKey = filename + ";" + metaboliteFragment;
-						double intensity = Double.parseDouble(csvRecord.get(MZminePeakOutputHeader.PEAK_HEIGHT.getColumnNumber()));
-						String name = StringUtils.remove(details[MZmineDetailsIdentfier.NAME.getIndex()], MZmineDetailsIdentfier.NAME.getIdentifier());
-						String[] nameTokens = StringUtils.split(name, "_");
-						int lastTokenIndex = nameTokens.length - 1;
-						int secondLastTokenIndex = nameTokens.length - 2;
-						c13Count = Integer.parseInt(nameTokens[secondLastTokenIndex]);
-						n15Count = Integer.parseInt(nameTokens[lastTokenIndex]);
-						singleIntensities[c13Count][n15Count] = singleIntensities[c13Count][n15Count] + intensity;
+		try {
+			for (String mzMineOutpuFilePath : mzMineOutpuFilePaths) {
+				File mzMinePeakData = new File(mzMineOutpuFilePath);
+				CSVParser parser;
+				parser = CSVParser.parse(mzMinePeakData, Charset.defaultCharset(), CSVFormat.RFC4180);
+				List<CSVRecord> records = parser.getRecords();
+				String filename = "";
+				String metaboliteFragment = "";
+				String formula = "";
+				int c13Count;
+				int n15Count;
+				String groupKey = "";
+				LinkedHashMap<String, Double[][]> groupKeyIntensitiesMap = new LinkedHashMap<>();
+				ArrayList<String> formulas = new ArrayList<>();
+				Double[][] singleIntensities = new Double[10][10];
+				for (CSVRecord csvRecord : records) {
+					if (csvRecord.getRecordNumber() == 1) {
+						String peakHeightHeader = csvRecord.get(MZminePeakOutputHeader.PEAK_HEIGHT.getColumnNumber());
+						filename = StringUtils.remove(peakHeightHeader, MZminePeakOutputHeader.PEAK_HEIGHT.getHeaderValue());
+						continue;
 					}
+					String detailStr = csvRecord.get(MZminePeakOutputHeader.SEPARATED_DETAILS.getColumnNumber());
+					String[] details = StringUtils.split(detailStr, ";");
+					formula = StringUtils.remove(details[MZmineDetailsIdentfier.FORMULA.getIndex()], MZmineDetailsIdentfier.FORMULA.getIdentifier());
+					metaboliteFragment = StringUtils.remove(details[MZmineDetailsIdentfier.ID.getIndex()], MZmineDetailsIdentfier.ID.getIdentifier());
+					metaboliteFragment = StringUtils.substringBefore(metaboliteFragment, "_");
+					// FIXME: this overwrites the group key each time
+					groupKey = filename + ";" + metaboliteFragment;
+					if (groupKeyIntensitiesMap.get(groupKey) == null) {
+						// TODO: choose a more elegant way for the size of the array
+						singleIntensities = new Double[10][10];
+						groupKeyIntensitiesMap.put(groupKey, singleIntensities);
+						formulas.add(formula);
+					}
+					Double intensity = Double
+							.parseDouble(csvRecord.get(MZminePeakOutputHeader.PEAK_HEIGHT.getColumnNumber()));
+					String name = StringUtils.remove(details[MZmineDetailsIdentfier.NAME.getIndex()], MZmineDetailsIdentfier.NAME.getIdentifier());
+					String[] nameTokens = StringUtils.split(name, "_");
+					int lastTokenIndex = nameTokens.length - 1;
+					int secondLastTokenIndex = nameTokens.length - 2;
+					c13Count = Integer.parseInt(nameTokens[secondLastTokenIndex]);
+					n15Count = Integer.parseInt(nameTokens[lastTokenIndex]);
+					singleIntensities[c13Count][n15Count] = singleIntensities[c13Count][n15Count] + intensity;
+				}
+				int entryCount = 0;
+				for (Entry<String, Double[][]> entry : groupKeyIntensitiesMap.entrySet()) {
+					String currentGroupKey = entry.getKey();
+					Double[][] currentSingleIntensities = entry.getValue();
 					for (int c = 0; c < 10; c++) {
 						for (int n = 0; n < 10; n++) {
-							if(singleIntensities[c][n] > 0) {
-								naCorrectionInputTable.addRow(groupKey, String.valueOf(singleIntensities[c][n]), String.valueOf(c), String.valueOf(n), formula);
+							if (currentSingleIntensities[c][n] > 0) {
+								naCorrectionInputTable.addRow(currentGroupKey,
+										String.valueOf(currentSingleIntensities[c][n]), String.valueOf(c),
+										String.valueOf(n), formulas.get(entryCount));
 							}
 						}
 					}
-					naCorrectionInputFilePath = FileWriterUtils.checkFilePath(naCorrectionInputFilePath, FileWriterUtils.CSV_EXTENSION);
+					entryCount++;
 				}
-				naCorrectionInputTable.writeToCsv("N/A", true, naCorrectionInputFilePath);
-			} catch (Exception e) {
-				e.printStackTrace();
+				naCorrectionInputFilePath = FileWriterUtils.checkFilePath(naCorrectionInputFilePath, FileWriterUtils.CSV_EXTENSION);
 			}
+			naCorrectionInputTable.writeToCsv("N/A", true, naCorrectionInputFilePath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	
+
+
 
 	public static void main(String[] args) {
 		String mzMineOutpuFilePath = "D:\\_Susi\\_mdc\\data\\mzmine_export\\gln\\13C15NglnPeaks.csv";
