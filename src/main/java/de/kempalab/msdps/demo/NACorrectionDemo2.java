7def439773886299d480f1cf8faf6d9fac6d76e3
susannefuerst <susannefuerst@freenet.de>
2019-05-29 09:53:02 +0200

    changed path

--- src/main/java/de/kempalab/msdps/demo/NACorrectionDemo2.java
+++ src/main/java/de/kempalab/msdps/demo/NACorrectionDemo2.java
@@ -5,7 +5,7 @@ import de.kempalab.msdps.fileconversion.NACorrectorFileConverter;
 
 public class NACorrectionDemo2 {
 	public static void main(String[] args) {
-		String pre = "D:\\data\\raw\\asn\\analysis\\";
+		String pre = "D:\\data\\raw\\asn\\120k\\analysis\\";
 		String[] mzMineOutpuFilePaths = {pre + "mixAexport.csv", pre + "mixBexport.csv", pre + "mixCexport.csv", pre + "mixDexport.csv"};
 		String naCorrectionInputFilePath = pre + "Converted.csv";
 		String naCorrectionOutputFilePath = pre + "Corrected.csv";
